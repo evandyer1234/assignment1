ea46305fc9e2573aa79eb53aab570440b5c7f3af
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-09-06 14:53:13 -0400

    my stuff

---
+++ UnityLab1/EdyerLab1/Assets/scripts/Cam.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public Camera camera;
+    public MoveTo spot;
+    public agent guy;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                MoveTo clone;
+                clone = Instantiate(spot, new Vector3 (hit.point.x, hit.point.y, hit.point.z), transform.rotation);
+                //guy.Target(hit.point);
+
+                //MoveTo MT = guy.gameObject.AddComponent(typeof(MoveTo)) as MoveTo;
+
+               
+                clone.endpos = hit.point;
+                clone.startpos = guy.gameObject.transform.position;
+                clone.guy = guy;
+                guy.points.Add(clone);
+                guy.stop = false;
+                
+            }
+        }
+    }
+}
---
+++ UnityLab1/EdyerLab1/Assets/scripts/addto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addto : MonoBehaviour
+{
+    public agent guy;
+
+    // Start is called before the first frame update
+    void awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
